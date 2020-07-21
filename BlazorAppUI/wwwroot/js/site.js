c2f79587c7e7ea16b11625133d79903c906c4268
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-21 15:55:14 -0400

    JavaScript Interop
    Calling JavaScript from C#

---
+++ BlazorAppUI/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿function saveMessage(firstName, lastName) {
+    //alert(firstName + ' ' + lastName + ' has been saved successfully!');
+
+    document.getElementById('divServerValidations').innerText = firstName + ' ' + lastName + ' has been saved successfully!';
+}
+
+function setFocusOnElement(element) {
+    element.focus();
+}
+
+function getCities() {
+    var cities = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia',
+        'San Diego', 'Dallas', 'San Jose', 'Austin', 'Jacksonville', 'Fort Worth', 'Columbos',
+        'Charlotte', 'Indianapolis', 'Seattle', 'Denver', 'Washington'];
+
+    return cities;
+}
